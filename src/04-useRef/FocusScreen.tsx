642bfc0f96c01087176753483ff3be5627ac70f2
Lea Muñoz <munozleandrom96@gmail.com>
2024-06-03 09:46:50 -0300

    Completado useRef

--- src/04-useRef/FocusScreen.tsx
+++ src/04-useRef/FocusScreen.tsx
@@ -1,6 +1,27 @@
+import { useRef } from "react";
 
 export const FocusScreen = () => {
-  return (
-    <div>FocusScreen</div>
-  )
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const onClickFocus = () => {
+        // console.log(inputRef);
+        inputRef.current?.select();
+    };
+
+
+    return (
+        <>
+            <h1>FocusScreen</h1>
+            <hr className="my-2" />
+            <input
+                ref={inputRef}
+                className="form-control m-2"
+                type="text"
+                placeholder="Ingrese su nombre"
+            />
+            
+
+            <button className="btn btn-primary m-2" onClick={onClickFocus}>Set Focus</button>
+        </>
+    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import ReactDOM from 'react-dom/client'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
+// import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
+import { FocusScreen } from './04-useRef/FocusScreen'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
     <div className='container'>
-      <MultipleCustomHook/>
+      <FocusScreen />
     </div>
   // </React.StrictMode>,
 )
