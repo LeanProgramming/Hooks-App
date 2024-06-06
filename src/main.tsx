d00306ac856185c0ee8df664d45bf50721c6c4c5
Lea Muñoz <munozleandrom96@gmail.com>
2024-06-06 09:35:11 -0300

    Instalacion de react routes dom. Creacion de MainApp, HomePage, LoginPage y AboutPage para manejo de rutas y useContext.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -13,14 +15,15 @@ import ReactDOM from 'react-dom/client'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
 // import './08-useReducer/intro-reducer'
-import { ToDoApp } from './08-useReducer/ToDoApp'
+// import { ToDoApp } from './08-useReducer/ToDoApp'
+import { MainApp } from './09-useContext/MainApp';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-    <div className='container'>
-      <ToDoApp />
-    </div>
-  // </React.StrictMode>,
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode>, */}
+  </BrowserRouter>
 )
